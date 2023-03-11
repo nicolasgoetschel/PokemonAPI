7ceafd98b0af29a89b7885cfce6fd4b4f602eb71
Nicolas Goetschel <nicgoe@hotmail.com>
2023-03-11 13:49:14 +0000

    random pokemon button working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonList from "./components/PokemonList";
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+      const data = await response.json();
+      setPokemonList(data.results);
+    }
+    fetchPokemon();
+  }, []);
+
+  const handleRandomClick = () => {
+    const randomIndex = Math.floor(Math.random() * pokemonList.length);
+    const randomPokemon = pokemonList[randomIndex];
+    setSelectedPokemon(randomPokemon);
+  };
+
   return (
-    <div>
-      <PokemonList />
+    <div className="App">
+      <h1>Pokemon List</h1>
+      <button onClick={handleRandomClick}>Random</button>
+      {selectedPokemon && (
+        <div>
+          <h2>{selectedPokemon.name}</h2>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              selectedPokemon.url.split("/")[6]
+            }.png`}
+            alt={selectedPokemon.name}
+          />
+        </div>
+      )}
     </div>
   );
 }
